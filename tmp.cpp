59b1e2b5c92872735f1cf40cc3c8eef0e6a3c4e6
Qingyuan Yang <quentinyang@hotmail.com>
2018-08-17 14:59:20 +0800

    Add files via upload

---
+++ tmp.cpp
@@ -0,0 +1,13 @@
+#include "daliyLiving.h"
+using namespace std;
+
+class CodingPeasant  : public FatIndoorsman {
+    private:
+        void Eat();
+        void Drink();
+        void Coding();
+        void VideoGames();
+    public:
+        virtual void ChineseValentinesDay();
+};
+
